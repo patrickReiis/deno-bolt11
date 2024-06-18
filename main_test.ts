ecfd5daef514fb0de794bad8188ebedbf25dbf9f
P. Reis <patrickpereirareal1@gmail.com>
2024-06-18 10:23:45 -0300

    test: getAmount

--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,12 @@
 import { assertEquals } from "jsr:@std/assert";
-import { add } from "./main.ts";
+import { getAmount } from './main.ts';
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+Deno.test('Amount is 200000', () => {
+  assertEquals(
+    getAmount(
+      'lnbc2u1pn8qatypp5dweqaltlry2vgpxxyc0puxnc50335yznevj2g46wrhfm2694lhgqhp576ekte7lhhtsxdk6tfvkpyp8gdk2xccmuccdxwjd0fqdh34wfseqcqzzsxqyz5vqsp5n44zva7xndawg5l2r9d85v0tszwejtfzkc7v90d6c7d3nsdt0qds9qxpqysgqx2v2artsxmnfkpapdm9f5pahjs8etlpe7kcjue2kffhjg3jrtearstjvenr6lxzhpw3es4hpchzzeet7ul88elurfmvr9v94v0655rgpy7m7r5',
+    ),
+    '200000',
+  );
 });
+
